d3974db9c9c0eddad189315cc248160eb900f92e
Leonid Volnitsky <Leonid@Volnitsky.com>
2008-08-08 01:40:31 +0300

    up

--- t-multimin.cc
+++ t-multimin.cc
@@ -6,19 +6,6 @@ using std::cerr;
 #include <gsl/gsl_multimin.h>
 #include "lvvlib.h"
 
-/* Paraboloid centered on (p[0],p[1]), with  scale factors (p[2],p[3]) and minimum p[4] */
-
-double my_f(const gsl_vector * v, void *params) {
-
-	double  x, y;
-	double *p = (double *) params;
-
-	x = gsl_vector_get(v, 0);
-	y = gsl_vector_get(v, 1);
-
-	return p[2] * (x - p[0]) * (x - p[0]) + p[3] * (y - p[1]) * (y - p[1]) + p[4];
-}
-
 class FMinimizer {  public:
 	FMinimizer (int N, double F(const gsl_vector*, void *), gsl_vector *x, void *p,  gsl_vector *ss,  bool _trace=false)
 	:	T (gsl_multimin_fminimizer_nmsimplex), trace(_trace)
@@ -39,6 +26,7 @@ class FMinimizer {  public:
 	void   iterate (double characteristic_size, int max_iter) {
 		int	test_status=GSL_CONTINUE;			// test_status:  GSL_SUCCESS==0; GSL_CONTINUE==-2; 
 
+		if (trace) FMT("# Itr  %10t Y   %20t Step  %30t X[0..]\n");
 		for  ( int iter=0;  iter<max_iter && (test_status==GSL_CONTINUE);   ++iter )   {
 
 			int  iterate_status = gsl_multimin_fminimizer_iterate(minimizer);
@@ -52,7 +40,11 @@ class FMinimizer {  public:
 			test_status = gsl_multimin_test_size(size, characteristic_size);
 			if (test_status == GSL_SUCCESS && trace )    cout  << "converged to minimum at\n";
 
-			if (trace) FMT("%5d %10.5d %10.5d %10.5d %10.5d\n") %iter %gsl_vector_get(minimizer->x, 0)  %gsl_vector_get(minimizer->x, 1)  %(minimizer->fval)  %size;
+			if (trace) { 
+				FMT("%5d %10.5d %10.5d") %iter   %(minimizer->fval)  %size;
+				for (int i=0; i<2; ++i) FMT("%10.5d") %gsl_vector_get(minimizer->x, i);
+				cout << endl;
+			}
 		}
 	}
 
@@ -62,8 +54,39 @@ class FMinimizer {  public:
 	bool				trace;
  };
 
-int main(void)  {
 
+// set contour; set isosamples 30; splot [] [-3:10] (1/(0.5+((x/2)**2-y)**2))/(0.5+(x/10)**2+(y/20)**2)
+double curved_trench(const gsl_vector * v, void *params) {
+	/* Paraboloid centered on (p[0],p[1]), with  scale factors (p[2],p[3]) and minimum p[4] */
+	double  x, y;
+	double *p = (double *) params;
+
+	x = gsl_vector_get(v, 0);
+	y = gsl_vector_get(v, 1);
+
+	return p[2] * (x - p[0]) * (x - p[0]) + p[3] * (y - p[1]) * (y - p[1]) + p[4];
+}
+
+double my_f(const gsl_vector * v, void *params) {
+	/* Paraboloid centered on (p[0],p[1]), with  scale factors (p[2],p[3]) and minimum p[4] */
+	double  x, y;
+	double *p = (double *) params;
+
+	x = gsl_vector_get(v, 0);
+	y = gsl_vector_get(v, 1);
+
+	return p[2] * (x - p[0]) * (x - p[0]) + p[3] * (y - p[1]) * (y - p[1]) + p[4];
+}
+
+double f(const gsl_vector * v, void *params) {
+	/* Paraboloid centered on (p[0],p[1]), with  scale factors (p[2],p[3]) and minimum p[4] */
+	const double  *x = gsl_vector_const_ptr(v, 0);
+	double        *p = (double *) params;
+	return p[2] * (x[0] - p[0]) * (x[0] - p[0]) + p[3] * (x[1] - p[1]) * (x[1] - p[1]) + p[4];
+}
+
+
+int	main(void)  {
 
 	/* Starting point */
 	gsl_vector *x = gsl_vector_alloc(2);
@@ -76,16 +99,16 @@ int main(void)  {
 
 	double  par[5] = { 1.0, 2.0, 10.0, 20.0, 30.0 };
 	cout << 
-		"# :gnuplot:  set view map; set dgrid3d; set key off; set contour surface; set cntrparam levels 20;   set isosample 20;"
+		"# :gnuplot:  set view 0,90,1.7;  set dgrid3d;  set key off;  set contour surface;  set cntrparam levels 20;  set isosample 20;"
 		"set zrange  [0:1000];  p0=1; p1=2; p2=10; p3=20; p4=30;"
 		"splot [-3:10] [-3:10]"
 			"p2*(x - p0)*(x-p0) + p3*(y-p1)*(y-p1) + p4,"
-			"\"pipe\" using 2:3:4:1 with labels;\n"
-			"# X, Y,  F(x),  step_size\n";
+			"\"pipe\" using 4:5:2:1 with labels;\n";
 
 
-	FMinimizer fm(2, my_f, x, par, ss, true);	
+	FMinimizer fm(2, f, x, par, ss, true);	
 	fm.iterate(1e-2, 100);
+
 	gsl_vector_free(x);
 	gsl_vector_free(ss);
 
