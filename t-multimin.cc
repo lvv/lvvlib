6e4e63240c10dad1ce7df4e420f883e32e2bd540
Leonid Volnitsky <Leonid@Volnitsky.com>
2008-08-11 01:11:56 +0300

    triv

--- t-multimin.cc
+++ t-multimin.cc
@@ -18,6 +18,7 @@ double f(const gsl_vector * gv, void *dummy) {
 // set contour; set isosamples 30; splot [] [-3:10] (1/(0.5+((x/2)**2-y)**2))/(0.5+(x/10)**2+(y/20)**2)
 double curved_trench(const gsl_vector * gv, void *params) {
 	#define  GP_F "splot [-1:8] [-3:8] (1/(0.5+((x/2)**2-y)**2))/(0.5+(x/10)**2+(y/20)**2),"
+	//#define  GP_F "unset view; splot [-1:8] [-3:8] "
 	const double  *x = gsl_vector_const_ptr(gv,0);
 	double 	y=(1/(0.5+pow2(pow2(x[0]/2)-x[1])))/(0.5+pow2(x[0]/10)+pow2(x[1]/20));
 								int static    cnt = 0;
@@ -30,14 +31,13 @@ double curved_trench(const gsl_vector * gv, void *params) {
 
 int	main(void)  {
 
-	double x0[2]    = {4.444,6.6666}; 	
-	double step0[2] = {0.5,0.5};
-	int    N        = sizeof(x0)/sizeof(x0[0]);
+	double	x0[2]    = {4.444,6.6666}; 	
+	double	step0[2] = {0.5,0.5};
+	int   	N        = sizeof(x0)/sizeof(x0[0]);
 
 	cout << 
-		"# :gnuplot:  set font \"arial,6\"; set view 0,90,1.7;  set dgrid3d;  set key off;  set contour surface;  set cntrparam levels 20;  set isosample 40;"
-		GP_F
-			"\"pipe\" using 4:5:2:1 with labels;\n";
+		"# :gnuplot:  set font \"arial,6\"; set view 0,0,1.7;  set dgrid3d;  set key off;  set contour surface;  set cntrparam levels 20;  set isosample 40;"
+		GP_F "\"pipe\" using 4:5:2:1 with labels;\n";
 
 	//f_minimizer 	fm(N, f, x0, step0);	
 	f_minimizer 	fm(N, curved_trench, x0, step0);	
