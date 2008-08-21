c10df38fccfba5b7b11061fbe229c254d52a7c58
Leonid Volnitsky <Leonid@Volnitsky.com>
2008-08-21 18:50:42 +0300

    arbitrary starting index works!

--- t-array.cc
+++ t-array.cc
@@ -3,7 +3,6 @@
 
 #include <cstddef>
 #include <stdexcept>
-//#include <boost/assert.hpp>
 #include <cassert>
 
 #include <iterator>
@@ -12,9 +11,9 @@
 #include <algorithm>
 using std::size_t;
 
-template < class T, size_t N > class array {
+template < class T, size_t N, int BEGIN=0> class array {
       public:
-	T elems[N];										   // fixed-size array of elements of type T
+	T elems[N];				
 
       public:
 	// type definitions
@@ -27,61 +26,43 @@ template < class T, size_t N > class array {
 
 	// iterator support
 	iterator				begin()				{ return elems; }
-	const_iterator				begin()			const	{ return elems; }
 	iterator				end()				{ return elems + N; }
-	const_iterator				end()			const	{ return elems + N; }
 
 	// reverse iterator support typedef
 	typedef std::reverse_iterator<iterator>		reverse_iterator;
-	typedef std::reverse_iterator<const_iterator>	const_reverse_iterator;
 
-	std::reverse_iterator<iterator>		rbegin()		{ return reverse_iterator(end()); }
-	const_reverse_iterator			rbegin()	const	{ return const_reverse_iterator(end()); }
-	reverse_iterator			rend()			{ return reverse_iterator(begin()); }
-	const_reverse_iterator			rend() const { return const_reverse_iterator(begin()); }
+	std::reverse_iterator<iterator>		rbegin()			{ return reverse_iterator(end()); }
+	reverse_iterator			rend()				{ return reverse_iterator(begin()); }
 
-	reference				operator[](size_type i)		{ assert(i < N && "out of range"); return elems[i]; }
-	const_reference				operator[](size_type i) const	{ assert(i < N && "out of range"); return elems[i]; }
+	reference				operator[](size_type i)		{ assert(i < N+BEGIN && "out of range"); return elems[i-BEGIN]; }
 	// at() with range check reference
-	reference				at(size_type i)			{ rangecheck(i); return elems[i]; }
-	const_reference				at(size_type i)		const	{ rangecheck(i); return elems[i]; }
+	reference				at(size_type i)			{ rangecheck(i); return elems[i+BEGIN]; }
 	reference				front()				{ return elems[0]; }
-	const_reference				front()			const	{ return elems[0]; }
-	reference				back()				{ return elems[N - 1]; }
-	const_reference				back()			const	{ return elems[N - 1]; }
+	reference				back()				{ return elems[N-1]; }
 	static size_type			size()				{ return N; }
 	static bool				empty()				{ return false; }
 	static size_type			max_size()			{ return N; }
 
 	enum { static_size = N };
 
-	// swap (note: linear complexity)
-	void swap(array < T, N > &y) { std::swap_ranges(begin(), end(), y.begin()); }
-
-	// direct access to data (read-only)
-	const T *				data()			const	{ return elems; }
-	T    *					data()				{ return elems; }
-
-	// use array as C array (direct read/write access to data)
-	T      *				c_array()			{ return elems; }
+	void					swap(array<T, N> &y)		{ std::swap_ranges(begin(), end(), y.begin()); }
+	T *					c_array()			{ return elems; }
 
 	// assignment with type conversion
 	template <typename T2>	array <T, N>	&operator=(const array < T2, N > &rhs) { std::copy(rhs.begin(), rhs.end(), begin()); return *this; }
-
 	// assign one value to all elements
 	void					assign(const T & value)		{ std::fill_n(begin(), size(), value); }
-
 	// check range (may be private because it is static)
-	static void				rangecheck(size_type i)		{ if (i >= size()) { throw std::out_of_range("array<>: index out of range"); } }
+	static void				rangecheck(size_type i)		{ if (i >= size()+BEGIN) { throw std::out_of_range("array<>: index out of range"); } }
 
 };
 
 
 // comparisons
 template<class T, size_t N> bool operator==(const array<T, N> &x, const array<T, N> &y) { return std::equal(x.begin(), x.end(), y.begin()); }
-template<class T, size_t N> bool operator <(const array<T, N> &x, const array<T, N> &y) { return std::lexicographical_compare(x.begin(), x.end(), y.begin(), y.end()); }
+template<class T, size_t N> bool operator< (const array<T, N> &x, const array<T, N> &y) { return std::lexicographical_compare(x.begin(), x.end(), y.begin(), y.end()); }
 template<class T, size_t N> bool operator!=(const array<T, N> &x, const array<T, N> &y) { return !(x == y); }
-template<class T, size_t N> bool operator> (const array<T, N> &x, const array<T, N> &y) { return y < x; }
+template<class T, size_t N> bool operator> (const array<T, N> &x, const array<T, N> &y) { return   y < x; }
 template<class T, size_t N> bool operator<=(const array<T, N> &x, const array<T, N> &y) { return !(y < x); }
 template<class T, size_t N> bool operator>=(const array<T, N> &x, const array<T, N> &y) { return !(x < y); }
 
@@ -91,20 +72,24 @@ template < class T, size_t N > inline void swap(array < T, N > &x, array < T, N 
 #endif				// BOOST_ARRAY_HPP ///////////////////////////////////////////////////////////////////////////////////////
 
 
-	#include <iostream>
-using namespace
-    std;
+#include <iostream>
+using namespace std;
+#include <lvv.h>
 
 int
 main()
 {
-	array < int,
-	    5 >
-	    a;
+	array < int, 5 > a;
+	array <int, 5, 1> aa;
 	ostream_iterator < int >
 	os(cout, " ");
 
-	for (unsigned i = 0; i < a.size(); ++i)
-		a[i] = i;
-	copy(a.begin(), a.end(), os);
+	unsigned i;
+	for (i = 0; i < a.size(); ++i)		a[i] = i;
+
+	for (i = 1; i < aa.size()+1; ++i)		aa[i] = i;
+	for (i = 1; i < aa.size()+1; ++i)		{PR1(aa[i])};
+
+	copy(a.begin(), a.end(), os);  	cout << endl;
+	copy(aa.begin(), aa.end(), os);	cout << endl;
 }
