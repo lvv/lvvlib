05af5283e389a73c14c327e5b59d2af26078338b
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-08-24 18:57:08 +0300

    check.h mods for scc

--- check.h
+++ check.h
@@ -1,44 +1,49 @@
-
 				
 				#ifndef LVV_CHECK_H
 				#define LVV_CHECK_H
 #include <iostream>
 	using std::cout;
 	using std::endl;
-#include <lvv/math.h>
+//#include <lvv/math.h>
 
-				namespace lvv {
+				namespace check {
 				
 ///////////////////////////////////////////////////////////////////////////////////// TEST MACROS
 static __attribute__((unused)) bool all_pass=true;
-//static __attribute__((unused)) bool result=false;
 static bool result=false;
 #define PASS "    pass    "
 #define FAIL ">>> FAIL <<<"
-#define RESULT   std::cout << (result ? PASS : FAIL) << " "  << __LINE__ << "\t\t";
+#define RESULT   std::cout << (check::result ? PASS : FAIL) << " "  << __LINE__ << "\t\t";
 
 
-#define CHECK(e)                result=(e);           all_pass = (all_pass && result); RESULT; std::cout << "check:        \t" #e  << std::endl;
+#define CHECK(e)                check::result=(e);           check::all_pass = check::all_pass  && check::result;   RESULT; std::cout << "check:        \t" #e  << std::endl;
 
-#define	CHECKeq		CHECK_ARE_EQUAL
-#define	CHECKneq	CHECK_NOT_EQUAL
-
+/*
 #define CHECK_ARE_FP_EQUAL(e1,e2)  result=(eq((e1),(e2)));  all_pass = (all_pass && result); RESULT; \
 	if (result) std::cout <<  "are equal:     \t" << #e1 "\t ==  " #e2  << std::endl; \
 	else	    std::cout <<  "are equal:     \t" << #e1 "\t ==  " #e2  << "     (values: " << (e1) << ",  " << (e2) << ")" << std::endl;
+*/
 
-#define CHECK_ARE_EQUAL(e1,e2)  result=((e1)==(e2));  all_pass = (all_pass && result); RESULT; \
-	if (result) std::cout <<  "are equal:     \t" << #e1 "\t ==  " #e2  << std::endl; \
-	else	    std::cout <<  "are equal:     \t" << #e1 "\t ==  " #e2  << "     (values: " << (e1) << ",  " << (e2) << ")" << std::endl;
+#define CHECK_TYPES_ARE_SAME(T,U)  check::result=std::is_same<T,U>::value;  check::all_pass = (check::all_pass && check::result); RESULT; \
+	if (check::result) std::cout <<  "same:     \t" << #T "\t ==  " #U  << std::endl; \
+	else	           std::cout <<  "same:     \t" << #T "\t ==  " #U  << "     (values: " << type2name<T>() << ",  " << type2name<U>() << ")" << std::endl;
 
-#define CHECK_NOT_EQUAL(e1,e2)  result=((e1)!=(e2));  all_pass = (all_pass && result); RESULT; \
-	if (result) std::cout <<  "are not equal: \t" << #e1 "\t !=  " #e2  << std::endl; \
-	else	    std::cout <<  "are not equal: \t" << #e1 "\t !=  " #e2  << "   values: " << (e1) << ",  " << (e2) <<  std::endl;
+#define CHECK_ARE_EQUAL(e1,e2)  check::result=((e1)==(e2));  check::all_pass = (check::all_pass && check::result); RESULT; \
+	if (check::result) std::cout <<  "are equal:     \t" << #e1 "\t ==  " #e2  << std::endl; \
+	else	           std::cout <<  "are equal:     \t" << #e1 "\t ==  " #e2  << "     (values: " << (e1) << ",  " << (e2) << ")" << std::endl;
+
+#define CHECK_NOT_EQUAL(e1,e2)  check::result=((e1)!=(e2));  check::all_pass = (check::all_pass && check::result); RESULT; \
+	if (check::result) std::cout <<  "are not equal: \t" << #e1 "\t !=  " #e2  << std::endl; \
+	else	           std::cout <<  "are not equal: \t" << #e1 "\t !=  " #e2  << "   values: " << (e1) << ",  " << (e2) <<  std::endl;
 
 
 #define CHECK_EXIT \
-	cout << (all_pass ? "\n------------ all pass ------------\n" : "\n!!!!!!!!!  SOME FAILED  !!!!!!!!\n"); \
-	exit(all_pass ? 0 : 1);
+	std::cout << (check::all_pass ? "\n------------ all pass ------------\n" : "\n!!!!!!!!!  SOME FAILED  !!!!!!!!\n"); \
+	exit(check::all_pass ? 0 : 1);
 
-				}; 	// namespace lvv
+// aliases
+#define	CHECKeq		CHECK_ARE_EQUAL
+#define	CHECKneq	CHECK_NOT_EQUAL
+
+				}; 	// namespace check
 				#endif  // LVV_CHECK_H
