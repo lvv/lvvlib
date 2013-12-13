7c3304ba795cdc3bf06dd83d23074491e73ad7b5
Leonid Volnitsky <Leonid@Volnitsky.com>
2013-12-14 00:49:58 +0200

    mmap_read API change

--- mmap.h
+++ mmap.h
@@ -20,56 +20,64 @@
 	struct  open_error: std::exception {};
 	struct  io_error: std::exception {};
 
-void * mmap_read  ( const char *name,  int flags=MAP_PRIVATE)   {
+template<int SHARING=MAP_PRIVATE> void * mmap_read_ptr  (const char *path, size_t& n)   {
 
-	//int 	src_fd = open(name, O_RDONLY);
-	int 	src_fd = open(name, O_RDWR);
+	int 	src_fd = open(path, O_RDWR);
 
 	if (src_fd  < 0) {
-		cerr  << "mmap_read error: couldn't open  \"" << name << "\"  file\n";
-		//exit(1);
+		cerr  << "mmap_read error: couldn't open  \"" << path << "\"  file\n";
 		throw no_file();
 	}
 	
 	struct stat sb;
 
+	// get file size
 	if (fstat(src_fd, &sb) < 0) {
-		cerr << "mmap_read error: couldn't stat  \"" << name << "\"   file\n";
+		cerr << "mmap_read error: couldn't stat  \"" << path << "\"   file\n";
 		close(src_fd);
 		throw io_error();
-		//exit(3);
 	}
 
-	void *p =  mmap(NULL, sb.st_size , PROT_READ | PROT_WRITE , flags, src_fd, 0);
-	//void *p =  mmap(NULL, sb.st_size , PROT_READ, flags, src_fd, 0);
+	n = sb.st_size;
+
+	void *p =  mmap(NULL, n, PROT_READ | PROT_WRITE , SHARING, src_fd, 0);
+	//void *p =  mmap(NULL, sb.st_size , PROT_READ, SHARING, src_fd, 0);
 
 	if  ( p == MAP_FAILED ) {
-		cerr  << "mmap_read error: couldn't mmap  \"" << name << "\"  file\n";
+		cerr  << "mmap_read error: couldn't mmap  \"" << path << "\"  file\n";
 		close(src_fd);
-		//exit(5);
 		throw io_error();
 	}
 
 	return p;
  }
 
-template<typename T>    T&   mmap_read(const char* name, int flags=MAP_SHARED)     { return  *(T*)mmap_read(name, flags); };
+template<typename MMAPED_TYPE, int SHARING=MAP_SHARED>    MMAPED_TYPE&   mmap_read(const char* path)               {
+	size_t unused;   
+	return  *(MMAPED_TYPE*) mmap_read_ptr<SHARING>(path, unused);
+ };
+
+template<typename REC_T,       int SHARING=MAP_SHARED>    REC_T*         mmap_read(const char* path, size_t& n)    {
+	size_t n_bytes;  
+	auto res =  (REC_T*)       mmap_read_ptr<SHARING>(path, n_bytes);
+	n = n_bytes/sizeof(REC_T);
+	return res;
+ };
 
 
-			template<typename T>
-void	mmap_write(const char* name, T &obj, size_t size=sizeof(T)) {
+template<typename T> void	mmap_write(const char* path, T &obj, size_t size=sizeof(T)) {
 
-	unlink(name);
+	unlink(path);
 
-	int	trg_fd = open(name, O_CREAT | O_RDWR, S_IRWXU);
+	int	trg_fd = open(path, O_CREAT | O_RDWR, S_IRWXU);
 	if (trg_fd  < 0) {
-		cerr  << "mmap_write error: couldn't open  \"" << name << "\"  file\n";
+		cerr  << "mmap_write error: couldn't open  \"" << path << "\"  file\n";
 		//exit(2);
 		throw open_error();
 	}
 
 	if (ftruncate(trg_fd, size) < 0) {
-		cerr << "mmap_write error: couldn't allocate space for  \"" << name << "\" file\n";
+		cerr << "mmap_write error: couldn't allocate space for  \"" << path << "\" file\n";
 		close(trg_fd);
 		//exit(4);
 		throw no_free_space();
@@ -78,7 +86,7 @@ void	mmap_write(const char* name, T &obj, size_t size=sizeof(T)) {
 
 	void *p = mmap(NULL, size, PROT_WRITE, MAP_SHARED, trg_fd, 0);
 	if ( p == MAP_FAILED ) {
-		cerr << "mmap_write error: couldn't mmap \"" << name << "\" file\n";
+		cerr << "mmap_write error: couldn't mmap \"" << path << "\" file\n";
 		close(trg_fd);
 		//exit(6);
 		throw io_error();
@@ -91,14 +99,14 @@ void	mmap_write(const char* name, T &obj, size_t size=sizeof(T)) {
        	mapping is less portable, the use of this option is discouraged. */
 
 	if (memcpy(p, &obj, size) < 0) {
-		cerr << "mmap_write error: couldn't memcpy() for \"" << name << "\" file\n";
+		cerr << "mmap_write error: couldn't memcpy() for \"" << path << "\" file\n";
 		close(trg_fd);
 		//exit(8);
 		throw std::exception();
 	}
 
 	if (munmap(p, size) < 0) {
-		cerr << "mmap_write error: couldn't munmap() for \"" << name << "\" file\n";
+		cerr << "mmap_write error: couldn't munmap() for \"" << path << "\" file\n";
 		close(trg_fd);
 		//exit(10);
 		throw std::exception();
--- t-mmap.cc
+++ t-mmap.cc
@@ -15,8 +15,9 @@ int main(int argc, char *argv[]) {
 
 	cout  << "\n\n   *** int  write, read  ***   \n";
 	int i=1;
+	size_t n;
 	                mmap_write<int> ("/tmp/_i.mmap", i);
-	int ii = *(int*)mmap_read       ("/tmp/_i.mmap");	// traditional  void * 
+	int ii = *(int*)mmap_read_ptr   ("/tmp/_i.mmap", n);	// traditional  void * 
 	int iii =       mmap_read<int>  ("/tmp/_i.mmap"); 	// templated ref
 	cout << "ii="  << ii  << endl;
 	cout << "iii=" << iii << endl;
@@ -28,22 +29,24 @@ int main(int argc, char *argv[]) {
 	mmap_write("/tmp/_fa.mmap", f);
 
 
-	float *ff, *fff;
-	//ff = (float*)mmap_read("/tmp/_fa.mmap", MAP_PRIVATE);
-	//ff = (float*)mmap_read("/tmp/_fa.mmap");
-	ff = (float*)mmap_read("/tmp/_fa.mmap");
-		cout << "ff[0]=" << ff[0] << endl;
-		cout << "ff[1]=" << ff[1] << endl;
-		cout << "ff[2]=" << ff[2] << endl;
+	float *F;
+	F = mmap_read<float>("/tmp/_fa.mmap",n);
+	
+	for (size_t i=0;  i<n;  ++i) {
+		cout << F[i] << ' ';
+	};  
+	cout << "   n=" << n << endl;
+
 
 	//ff[1] = 11;     cout << "can modify _fa map "  << endl;
 
 	cout << "\n\n   *** double lvv::array write, read ***  \n";
-	typedef array<double, 3> array_t;
+	typedef lvv::array<double, 3> array_t;
 	array_t  d={{0.11, 0.22, 0.33}};
 	mmap_write<array_t> ("/tmp/_da.mmap", d); 	// size is taken from array_t 
 	array_t&  dd = mmap_read<array_t> ("/tmp/_da.mmap"); 
-	cout << "array<double,3> d   " << d ;
+	cout << "array<double,3> d  =" << d  << endl;
+	cout << "array<double,3> dd =" << dd  << endl;
 
 	return 0;
 }
